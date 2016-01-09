5b058c628cc4ae2545b7b5dd4494229878feed67
CHLLHC <chl41993@gmail.com>
2016-01-09 23:50:47 +0800

    sdn-controller-routing-protocol.cc initialized

---
+++ ns-3/src/sdn/model/sdn-controller-routing-protocol.cc
@@ -0,0 +1,93 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2016 Haoliang Chen
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Haoliang Chen <chl41993@gmail.com>
+ */
+
+
+///
+/// \brief Implementation of SDN agent on controller side 
+/// and related classes.
+///
+/// This is the main file of this software because SDN's behaviour is
+/// implemented here.
+///
+
+#define NS_LOG_APPEND_CONTEXT                                   \
+  if (GetObject<Node> ()) { std::clog << "[node " << GetObject<Node> ()->GetId () << "] "; }
+
+
+#include "sdn-controller-routing-protocol.h"
+#include "ns3/socket-factory.h"
+#include "ns3/udp-socket-factory.h"
+#include "ns3/simulator.h"
+#include "ns3/log.h"
+#include "ns3/names.h"
+#include "ns3/inet-socket-address.h"
+#include "ns3/ipv4-routing-protocol.h"
+#include "ns3/ipv4-routing-table-entry.h"
+#include "ns3/ipv4-route.h"
+#include "ns3/boolean.h"
+#include "ns3/uinteger.h"
+#include "ns3/enum.h"
+#include "ns3/trace-source-accessor.h"
+#include "ns3/ipv4-header.h"
+
+/********** Useful macros **********/
+
+///
+/// \brief Gets the delay between a given time and the current time.
+///
+/// If given time is previous to the current one, then this macro returns
+/// a number close to 0. This is used for scheduling events at a certain moment.
+///
+#define DELAY(time) (((time) < (Simulator::Now ())) ? Seconds (0.000001) : \
+                     (time - Simulator::Now () + Seconds (0.000001)))
+
+
+
+
+
+
+/********** Miscellaneous constants **********/
+
+/// Maximum allowed jitter.
+#define SDN_MAXJITTER          (m_helloInterval.GetSeconds () / 4)
+/// Random number between [0-SDN_MAXJITTER] used to jitter SDN packet transmission.
+#define JITTER (Seconds (m_uniformRandomVariable->GetValue (0, SDN_MAXJITTER)))
+
+
+#define SDN_PORT_NUMBER 419
+/// Maximum number of messages per packet.
+#define SDN_MAX_MSGS    64
+
+namespace ns3 {
+namespace sdn {
+
+NS_LOG_COMPONENT_DEFINE ("SdnControllerRoutingProtocol");
+
+
+/********** SDN controller class **********/
+
+NS_OBJECT_ENSURE_REGISTERED (RoutingProtocol);
+
+
+
+} // namespace olsr
+} // namespace ns3
+
+
