ecc3b2e92a5912e3fba6051e29f85b5d81c096b4
CHLLHC <chl41993@gmail.com>
2016-02-23 16:49:40 +0800

    helper done, but ProcessHM and SendRoutingTable haven't

--- ns-3/src/sdn/helper/sdn-helper.cc
+++ ns-3/src/sdn/helper/sdn-helper.cc
@@ -26,5 +26,104 @@
 
 namespace ns3 {
 
+SdnHelper::SdnHelper ()
+{
+  m_agentFactory.SetTypeId ("ns3::sdn::RoutingProtocol");
+}
+
+SdnHelper::SdnHelper (const SdnHelper &o)
+  : m_agentFactory (o.m_agentFactory)
+{
+  m_mobility = o.m_mobility;
+  m_interfaceExclusions = o.m_interfaceExclusions;
+}
+
+SdnHelper*
+SdnHelper::Copy () const
+{
+  return new SdnHelper (*this);
+}
+
+void
+SdnHelper::ExcludeInterface (Ptr<Node> node, uint32_t interface)
+{
+  std::map< Ptr<Node>, std::set<uint32_t> >::iterator it = m_interfaceExclusions.find (node);
+
+  if(it == m_interfaceExclusions.end ())
+    {
+      std::set<uint32_t> interfaces;
+      interfaces.insert (interface);
+
+      m_interfaceExclusions.insert (std::make_pair (node, std::set<uint32_t> (interfaces) ));
+    }
+  else
+    {
+      it->second.insert (interface);
+    }
+}
+
+Ptr<Ipv4RoutingProtocol>
+SdnHelper::Create (Ptr<Node> node) const
+{
+  Ptr<sdn::RoutingProtocol> agent = m_agentFactory.Create<sdn::RoutingProtocol> ();
+
+  std::map<Ptr<Node>, std::set<uint32_t> >::const_iterator it = m_interfaceExclusions.find (node);
+
+  if(it != m_interfaceExclusions.end ())
+    {
+      agent->SetInterfaceExclusions (it->second);
+    }
+
+  node->AggregateObject (agent);
+  return agent;
+}
+
+void
+SdnHelper::Set (std::string name, const AttributeValue &value)
+{
+  m_agentFactory.Set (name, value);
+}
+
+int64_t
+SdnHelper::AssignStreams (NodeContainer c, int64_t stream)
+{
+  int64_t currentStream = stream;
+  Ptr<Node> node;
+  for (NodeContainer::Iterator i = c.Begin (); i != c.End (); ++i)
+    {
+      node = (*i);
+      Ptr<Ipv4> ipv4 = node->GetObject<Ipv4> ();
+      NS_ASSERT_MSG (ipv4, "Ipv4 not installed on node");
+      Ptr<Ipv4RoutingProtocol> proto = ipv4->GetRoutingProtocol ();
+      NS_ASSERT_MSG (proto, "Ipv4 routing not installed on node");
+      Ptr<sdn::RoutingProtocol> sdn = DynamicCast<sdn::RoutingProtocol> (proto);
+      if (sdn)
+        {
+          currentStream += sdn->AssignStreams (currentStream);
+          continue;
+        }
+      // Sdn may also be in a list
+      Ptr<Ipv4ListRouting> list = DynamicCast<Ipv4ListRouting> (proto);
+      if (list)
+        {
+          int16_t priority;
+          Ptr<Ipv4RoutingProtocol> listProto;
+          Ptr<sdn::RoutingProtocol> listSdn;
+          for (uint32_t i = 0; i < list->GetNRoutingProtocols (); i++)
+            {
+              listProto = list->GetRoutingProtocol (i, priority);
+              listSdn = DynamicCast<sdn::RoutingProtocol> (listProto);
+              if (listSdn)
+                {
+                  currentStream += listSdn->AssignStreams (currentStream);
+                  break;
+                }
+            }
+        }
+    }
+  return (currentStream - stream);
+
+}
+
 
 } // namespace ns3
